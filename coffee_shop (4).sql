858ece1bcce4dbc74e794526d4a7ddb01aa431f9
Tin Wongcharoo <37769054+BlueFish01@users.noreply.github.com>
2022-11-24 18:24:11 +0700

    Update coffee_shop (4).sql

--- coffee_shop (4).sql
+++ coffee_shop (4).sql
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `coffee_shop2`
+-- Database: `coffee_shop`
 --
-CREATE DATABASE IF NOT EXISTS `coffee_shop2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
-USE `coffee_shop2`;
+CREATE DATABASE IF NOT EXISTS `coffee_shop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `coffee_shop`;
 
 -- --------------------------------------------------------
 
